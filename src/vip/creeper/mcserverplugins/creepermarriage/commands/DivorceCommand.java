052098b8e64ca7ba1c5464751cac57c233d268ad
BuildTools <unconfigured@null.spigotmc.org>
2017-04-22 19:36:04 +0800

    up

--- src/vip/creeper/mcserverplugins/creepermarriage/commands/DivorceCommand.java
+++ src/vip/creeper/mcserverplugins/creepermarriage/commands/DivorceCommand.java
@@ -22,8 +22,6 @@ public class DivorceCommand implements ICommand {
 		MarriagePlayer mp=new MarriagePlayer(playerName);
 		Player partner=Bukkit.getPlayer(mp.getPartnerName());
 		mp.divorce();
-		//testt
-		Util.sendMsg(player, "");
 		Util.sendMsg(player, "&c离婚了QAQ.");
 		Util.sendMsg(partner, "&e"+playerName+" &c和你离婚了...");
 		return true;
