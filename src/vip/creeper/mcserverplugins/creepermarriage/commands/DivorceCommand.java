8590f05181686d66e8ae9b5f3b46aeee052e369d
July_ss <creeper.vip>
2017-04-22 19:23:42 +0800

    update

--- src/vip/creeper/mcserverplugins/creepermarriage/commands/DivorceCommand.java
+++ src/vip/creeper/mcserverplugins/creepermarriage/commands/DivorceCommand.java
@@ -22,6 +22,7 @@ public class DivorceCommand implements ICommand {
 		MarriagePlayer mp=new MarriagePlayer(playerName);
 		Player partner=Bukkit.getPlayer(mp.getPartnerName());
 		mp.divorce();
+		//
 		Util.sendMsg(player, "&c离婚了QAQ.");
 		Util.sendMsg(partner, "&e"+playerName+" &c和你离婚了...");
 		return true;
